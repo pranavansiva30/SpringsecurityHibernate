fbae8fb55e7872738b41ff80be7dd2c3e63a731a
pranavansiva30 <pranavan.siva.ps@gmail.com>
2017-02-03 14:39:59 +0530

    Springsecurity with hibernate using annotation

---
+++ src/main/java/com/mkyong/users/dao/UserDaoImpl.java
@@ -0,0 +1,42 @@
+package com.mkyong.users.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import com.mkyong.users.model.User;
+
+@Repository("userDao")
+public class UserDaoImpl implements UserDao {
+    @Autowired
+	private SessionFactory sessionFactory;
+
+	@SuppressWarnings("unchecked")
+	public User findByUserName(String username) {
+
+		List<User> users = new ArrayList<User>();
+
+		users = getSessionFactory().getCurrentSession().createQuery("from User where username=?")
+				.setParameter(0, username).list();
+
+		if (users.size() > 0) {
+			return users.get(0);
+		} else {
+			return null;
+		}
+
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+}
